0bebab0e326b645af9a09826a53cf3a6d1bc4f4a
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-10 21:05:42 +0000

    Automatically backed up by Learn

--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -4,4 +4,4 @@ puts "What year were you born?"
 birth_year = get.TOPLEVEL_BINDING
 users_age = current_age_for_birth_year(birth_year)
 
-puts "Ypu are: " + users_age.to_s + " years old."
+puts "You are: " + users_age.to_s + " years old."
